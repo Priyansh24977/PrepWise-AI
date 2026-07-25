ec7af51dbd8e094f52b3dd29d92f51a117977f4f
Priyansh Dwivedi <priyanshdwivedi15@gmail.com>
2026-07-25 20:44:38 +0530

    Use environment variable for API URL

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const api =axios.create({
-    baseURL:"http://localhost:8080",
-    withCredentials:true
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
 });
 
 export async function register({username,email,password}){
