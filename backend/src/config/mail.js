eb2fee1c820f78748194b207cfe693e1313e11ca
Priyansh Dwivedi <priyanshdwivedi15@gmail.com>
2026-07-25 23:29:20 +0530

    fix mail.js

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,17 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-        
-    }
-    
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export default transporter;
