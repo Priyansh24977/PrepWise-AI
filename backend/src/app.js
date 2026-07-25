e11b70715f513d8bb6e548d9daf679e4c12ec75c
Priyansh Dwivedi <priyanshdwivedi15@gmail.com>
2026-07-25 22:04:01 +0530

    Configure CORS for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,9 +12,12 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
-}))
+    origin:
+        process.env.NODE_ENV === "production"
+            ? process.env.FRONTEND_URL
+            : "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use('/api/auth',authRouter);
 app.use('/api/interview/',interviewRouter)
