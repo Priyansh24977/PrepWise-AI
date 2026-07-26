78a9bc61ea33b3b683703c8dee10bacfdf86b5ef
Priyansh Dwivedi <priyanshdwivedi15@gmail.com>
2026-07-26 14:35:57 +0530

    fix error

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -5,12 +5,15 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.BREVO_SMTP_LOGIN,
     pass: process.env.BREVO_SMTP_PASSWORD,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export default transporter;
