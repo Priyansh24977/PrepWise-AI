c43545bc11f81fa250c28f291eb1f63ec0e8dd42
Priyansh Dwivedi <priyanshdwivedi15@gmail.com>
2026-08-24 22:21:45 +0530

    fix: Correct ESM import order in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import dns from 'dns';
 dns.setServers([
   '8.8.8.8',
   '8.8.4.4'
 ]);
-import dotenv from "dotenv";
-dotenv.config();
 
 import app from './src/app.js';
 import connectToDB from './src/config/database.js';
-import dns from 'dns';
-
-
-
-
-
 
 connectToDB();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
